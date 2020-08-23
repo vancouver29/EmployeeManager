f8361b3f2523fec6e01d4384a0e5232582e0f650
vancouver29 <vancouver2990@gmail.com>
2020-08-23 12:21:41 +0200

    created backend project

---
+++ backend/src/main/java/com/vancouver/web/User.java
@@ -0,0 +1,18 @@
+package com.vancouver.web;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String Name;
+    private String Email;
+    private String Password;
+}
